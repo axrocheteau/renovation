ccfa675dd91e2658973e22baed7fc5b8751c9b87
axrocheteau <axrocheteau>
2023-05-04 07:30:41 +0000

    Create housing

--- Create tables/Create Housing.py
+++ Create tables/Create Housing.py
@@ -21,12 +21,37 @@ spark = SparkSession \
 # COMMAND ----------
 
 # load df
-df = spark.sql("SELECT * FROM datalake.codebook")
-display(df)
+tremi = spark.sql("SELECT * FROM datalake.tremi")
+display(tremi)
+
+# COMMAND ----------
+
+housing = (tremi.select(   
+        F.col('Respondent_Serial').alias('id_owner'),
+        F.when(F.col('cd_postal') < 10000, F.concat(F.lit("0"), F.col('cd_postal').cast('string')))
+            .otherwise(F.col('cd_postal').cast('string'))
+            .alias('cd_postal'),
+        F.col('main_Q101').alias('type'),
+        F.col('main_Q102').alias('construction_date'),
+        F.col('main_Q103').alias('heating_system'),
+        F.col('main_Q104').alias('hot_water_system'),
+        F.col('main_Q41q42').alias('surface'),
+        F.col('main_Q1_97').alias('has_done_renov'),
+        F.col('main_Q43').alias('DPE_before'),
+        F.col('main_Q52').alias('thermal_comfort'),
+        F.col('main_Q53').alias('energy_reduction'),
+        F.col('main_Q38').alias('adjoining'),
+        F.col('main_Q39').alias('n_floors'),
+        F.col('main_Q40').alias('floor_nb')
+    )
+    .dropDuplicates()
+    .select(F.monotonically_increasing_id().alias('id_housing'),"*")
+)
+display(housing)
 
 # COMMAND ----------
 
 # save as table
-Dictionnary.write.mode('overwrite')\
+housing.write.mode('overwrite')\
         .format("parquet") \
-        .saveAsTable("Gold.Dictionnary")
+        .saveAsTable("Gold.Housing")
