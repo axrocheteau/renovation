bcdbd75535557983f33bdbae7205a55897859fab
axrocheteau <axrocheteau>
2023-06-13 12:46:00 +0000

    open_files optimisation

--- Import files/Open files.py
+++ Import files/Open files.py
@@ -24,56 +24,47 @@ spark.sql("CREATE DATABASE IF NOT EXISTS Gold")
 
 # create database
 # File location and type
-file_location_array = ["/FileStore/tables/Communes.csv",
-                        "/FileStore/tables/Departements.csv",
-                        "/FileStore/tables/PC_DP_creant_logements_2013_2016.csv",
-                        "/FileStore/tables/Regions.csv",
-                        "/FileStore/tables/TREMI_2017_CodeBook_public8.txt",
-                        "/FileStore/tables/TREMI_2017_Résultats_enquête_bruts.csv",
-                        "/FileStore/tables/anciennes_nouvelles_regions.csv",
-                        "/FileStore/tables/code_commune.csv",
-                        "/FileStore/tables/conso_elec_gaz_annuelle_par_secteur_dactivite_agregee_commune__1_.csv",
-                        "/FileStore/tables/donnees_synop_essentielles_omm.csv",
-                        "/FileStore/tables/dpe_france.csv",
-                        "/FileStore/tables/permis_amenager.csv",
-                        "/FileStore/tables/permis_demolir.csv",
-                        "/FileStore/tables/dep_limitrophe.csv"          
-]
+# create database
+# File location and type
+info_array = [
+    {"location" : "/FileStore/tables/Communes.csv", "name": "pop_commune", "delimiter": ";"},
+    {"location" : "/FileStore/tables/Departements.csv", "name": "pop_department", "delimiter": ";"},
+    {"location" : "/FileStore/tables/PC_DP_creant_logements_2013_2016.csv", "name": "construction_licence", "delimiter": ";"},
+    {"location" : "/FileStore/tables/Regions.csv", "name": "pop_region", "delimiter": ";"},
+    {"location" : "/FileStore/tables/TREMI_2017_CodeBook_public8.txt", "name": "codebook", "delimiter": "\t"},
+    {"location" : "/FileStore/tables/TREMI_2017_Résultats_enquête_bruts.csv", "name": "tremi", "delimiter": ";"},
+    {"location" : "/FileStore/tables/anciennes_nouvelles_regions.csv", "name": "former_new_region", "delimiter": ";"},
+    {"location" : "/FileStore/tables/code_commune.csv", "name": "code_commune", "delimiter": ";"},
+    {"location" : "/FileStore/tables/conso_elec_gaz_annuelle_par_secteur_dactivite_agregee_commune__1_.csv", "name": "elec", "delimiter": ";"},
+    {"location" : "/FileStore/tables/donnees_synop_essentielles_omm.csv", "name": "weather", "delimiter": ";"},
+    {"location" : "/FileStore/tables/dpe_france.csv", "name": "dpe_france", "delimiter": ","},
+    {"location" : "/FileStore/tables/permis_amenager.csv", "name": "development_licence", "delimiter": ";"},
+    {"location" : "/FileStore/tables/permis_demolir.csv", "name": "destruction_licence", "delimiter": ";"},
+    {"location" : "/FileStore/tables/dep_limitrophe.csv", "name": "neighbouring_dep", "delimiter": ";"}
+]      
+
 file_type = "csv"
-name_array = ["pop_commune",
-                "pop_department",
-                "construction_licence",
-                "pop_region",
-                "codebook",
-                "tremi",
-                "former_new_region",
-                "code_commune",
-                "elec",
-                "weather",
-                "dpe_france",
-                "development_licence",
-                "destruction_licence",
-                "neighbouring_dep"
-]
 
 # CSV options
 infer_schema = "true"
 first_row_is_header = "true"
-delimiter_array = [";", ";", ";", ";", "\t", ";", ";", ";", ";", ";", ",", ";", ";", ";"]
 
 # The applied options are for CSV files. For other file types, these will be ignored.
-for file_location, delimiter, name in zip(file_location_array, delimiter_array, name_array):
+for file in info_array:
+    delimiter = file["delimiter"]
+    file_location = file["location"]
+    name = file["name"]
     df = spark.read.format(file_type) \
         .option("inferSchema", infer_schema) \
         .option("header", first_row_is_header) \
         .option("sep", delimiter) \
         .load(file_location)
     if name == "weather":
-        new_column_name_list= [name.replace(',','') for name in df.columns]
+        new_column_name_list= [column_name.replace(',','') for column_name in df.columns]
         df = df.toDF(*new_column_name_list)
-    df.write.mode('overwrite')\
+    df.write.mode("overwrite")\
         .format("parquet") \
-        .saveAsTable(f"Datalake.{name}")
+        .saveAsTable(f"datalake.{name}")
 
 # COMMAND ----------
 
