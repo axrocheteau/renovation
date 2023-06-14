489c69e44c993bd5997ef56662ab5d46bba3ede3
axrocheteau <axrocheteau>
2023-06-14 09:56:13 +0000

    import on notebooks

---
+++ Create tables/Gold/Municipality_info.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Municipality info
+
+# COMMAND ----------
+
+from os.path import abspath
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+# spark session to warehouse
+warehouse_location = abspath('spark-warehouse')
+spark = SparkSession \
+    .builder \
+    .appName("SparkByExamples.com") \
+    .config("spark.sql.warehouse.dir", warehouse_location) \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+# COMMAND ----------
+
+# load df
+tremi = spark.sql("SELECT * FROM datalake.tremi")
+Dictionnary = spark.sql("SELECT * FROM Silver.Dictionary")
